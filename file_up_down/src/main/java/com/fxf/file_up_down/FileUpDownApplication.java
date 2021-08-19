99d697437a129953f010365957f49e11c494db69
fanzhijie <2099512771@qq.com>
2021-08-19 13:48:04 +0800

    基于Restful风格的文件上传与下载补充

---
+++ file_up_down/src/main/java/com/fxf/file_up_down/FileUpDownApplication.java
@@ -0,0 +1,13 @@
+package com.fxf.file_up_down;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUpDownApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileUpDownApplication.class, args);
+    }
+
+}
